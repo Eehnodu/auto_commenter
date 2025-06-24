e067d2af5a1f5be4614a72381660f2a517726057
Nodu <Eehnodu@gmail.com>
2025-06-24 17:00:02 +0900

    feat: tistory

--- auto_commenter/background.js
+++ auto_commenter/background.js
@@ -1,9 +1,22 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.type === "start_search") {
-    chrome.storage.local.get("keyword", ({ keyword }) => {
-      const encoded = encodeURIComponent(keyword);
-      const searchUrl = `https://section.blog.naver.com/Search/Post.naver?query=${encoded}`;
-      chrome.tabs.create({ url: searchUrl });
-    });
+    chrome.storage.local.get(
+      ["keyword", "platform"],
+      ({ keyword, platform }) => {
+        const encoded = encodeURIComponent(keyword);
+
+        let searchUrl = "";
+
+        if (platform === "naver") {
+          searchUrl = `https://section.blog.naver.com/Search/Post.naver?query=${encoded}`;
+        } else if (platform === "tistory") {
+          searchUrl = `https://search.daum.net/search?w=blog&q=${encoded}`;
+        }
+
+        if (searchUrl) {
+          chrome.tabs.create({ url: searchUrl });
+        }
+      }
+    );
   }
 });
